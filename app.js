679609c22eccbff4d2d195d2c179c7a3e8c07939
Arnor Ameti <arnameti@gmail.com>
2022-05-17 12:59:29 +0200

    Finished Project

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
 // http://numbersapi.com/
 
+const factPara = document.querySelector(".fact");
+const factInput = document.querySelector("input");
+const factButton = document.querySelector("button");
+
+const displayFact = (fact) => {
+  factPara.innerHTML = fact;
+};
+
+const fetchFact = async (number) => {
+  const factUrl = "http://numbersapi.com/";
+
+  const res = await fetch(`${factUrl}${number}`);
+  const data = await res.text();
+
+  displayFact(data);
+};
+
+factButton.addEventListener("click", () => {
+  const number = factInput.value;
+  const numberFact = +number;
+
+  fetchFact(numberFact);
+});
